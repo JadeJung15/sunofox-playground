8713b7ed48d569c3883c27911b0ab46abea64731
jadejung15 <jadejung15@gmail.com>
2026-03-09 00:17:04 +0000

    v8.5.3 배포

--- js/site-config.js
+++
@@ -1,3 +0,0 @@
-window.__SEVENCHECK_CONFIG__ = window.__SEVENCHECK_CONFIG__ || {};
-window.__SEVENCHECK_CONFIG__.firebaseProjectId = window.__SEVENCHECK_CONFIG__.firebaseProjectId || 'sunofoxplayground-backend';
-window.__SEVENCHECK_CONFIG__.firebaseRegion = window.__SEVENCHECK_CONFIG__.firebaseRegion || 'asia-northeast3';
